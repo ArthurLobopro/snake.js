17b2d5924e40a96264d02f1bb2cdb136e469ed36
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-21 16:04:31 -0300

    Funções de leitura das configurações padrão

---
+++ src/Settings.js
@@ -0,0 +1,19 @@
+const getDefaultSnake = async ()=> {
+    let { snake } = 
+    await fetch("../settings/default.json")
+    .then( data => {
+        return data.json()
+    })
+    return snake
+}
+
+const getDefaultGame = async () => {
+    let { game } = 
+    await fetch("../settings/default.json")
+    .then( data => {
+        return data.json()
+    })
+    return game
+}
+
+export { getDefaultSnake, getDefaultGame }
