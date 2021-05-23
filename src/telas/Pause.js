b71c48861127c456ef8c54d98d00c93ef69b30aa
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-23 15:49:17 -0300

    Tela de pause adicionada

---
+++ src/telas/Pause.js
@@ -0,0 +1,19 @@
+const get = id => document.getElementById(id)
+const gameDiv = get('game')
+const tela = get('tela')
+export default function viewPause({play, newGame}) {
+    const fieldset = document.createElement('fieldset')
+    fieldset.innerHTML = `
+    <legend>Pause</legend>
+    <button id="continue">CONTINUE</button>
+    <button id="new-game">NEW GAME</button>`
+    tela.insertBefore(fieldset, gameDiv)
+    get('continue').onclick = () => {
+        tela.removeChild(fieldset)
+        setTimeout( play, 150)
+    }
+    get('new-game').onclick = () => {
+        tela.removeChild(fieldset)
+        setTimeout( newGame, 150)
+    }
+}
