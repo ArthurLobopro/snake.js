9774cd65e8460a1fd7ee261d12a6341d41def8f3
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-25 16:00:31 -0300

    Create main.js

---
+++ electron/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow} = require('electron')
+const path = require('path')
+
+require('./header/header-actions-main.js')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 865,
+        height: 625,
+        minWidth: 865,
+        minHeight: 625,
+        frame: false,
+        icon: path.join( __dirname, "../assets/icon32.png"),
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    win.setMenuBarVisibility(null)
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+// Faz com que o programa não inicie várias vezes durante a instalação
+// if (require('electron-squirrel-startup')){
+//     return app.quit();
+// }
