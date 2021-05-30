cc7ea75c3c9fbb749659da856fae688e150bcb55
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-30 00:13:20 -0300

    menu de configuração de velocidade

---
+++ src/telas/configs/velocidade.js
@@ -0,0 +1,61 @@
+
+const tela = document.getElementById('tela')
+export default async function velocidade(game){
+    const {velocidade} = game
+    const full = document.createElement('div')
+    full.classList.add('full')
+    full.innerHTML = `
+    <fieldset id="">
+        <legend class="title">Velocidade</legend>        
+            <div class="text">
+                <div class="line">
+                    <div>Lento</div>
+                    <div class="check" data-check="${velocidade === 300 ? true : false}" data-value="300"></div>
+                </div>
+                <div class="line">
+                    <div>Médio</div>
+                    <div class="check" data-check="${velocidade === 200 ? true : false}" data-value="200"></div>
+                </div>
+                <div class="line">
+                    <div>Rápido</div>
+                    <div class="check" data-check="${velocidade === 100 ? true : false}" data-value="100"></div>
+                </div>
+            </div>
+            <div class="buttons">
+                <button value="1">
+                    OK
+                </button>
+                <button class="cancel" value="0">
+                    Cancelar
+                </button>
+            </div>
+    </fieldset>`
+    const fieldset = full.querySelector('fieldset')
+    const checks = fieldset.querySelectorAll('.check')
+    checks.forEach( e => {
+        e.onclick = event => {
+            const target = event.target
+           
+            document.querySelectorAll('.check').forEach( e => {
+                e.dataset.check = "false"
+            })
+           
+            target.dataset.check = "true"
+        }
+    })
+    tela.appendChild(full)
+    const buttons = fieldset.querySelectorAll('button')
+    return new Promise(resolve => {
+        buttons.forEach( e => {
+            e.onclick = event =>{
+                tela.removeChild(full)
+                if(event.target.value == "1"){
+                    const value = Number(Array.from(checks).find( e => e.dataset.check === "true").dataset.value)
+                    resolve(['velocidade', value])
+                }else{
+                    resolve([])
+                }
+            }
+        })
+    })
+}
