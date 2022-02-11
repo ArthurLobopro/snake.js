a5c1d5e023a015232a61e96bd13d22dd764ee070
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-02-10 22:43:46 -0300

    refactor: add Screen module

---
+++ src/render/Screens/Screen.js
@@ -0,0 +1,46 @@
+import navigation from "./navigation.js"
+
+const container = document.getElementById("container")
+
+export class Screen {
+    buildFunction(){}
+    
+    addNavigation() {
+        window.onkeydown = event => navigation[event.key]?.(this.screen)
+    }
+
+    removeNavigation(){
+        window.onkeydown = null
+    }
+
+    reset() {
+        this.screen = this.buildFunction()
+    }
+
+    show(navigation = true) {
+        container.appendChild(this.screen)
+        if(navigation){
+            this.addNavigation()
+        }
+    }
+
+    hide() {
+        container.removeChild(this.screen)
+    }
+
+    close() {
+        this.hide()
+        this.reset()
+    }
+}
+
+export class ConfigScreenBase extends Screen {
+    constructor(){
+        super()
+    }
+
+    show() {
+        this.reset()
+        super.show(false)
+    }
+}
