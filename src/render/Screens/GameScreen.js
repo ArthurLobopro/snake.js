b3206ff6c7fd7c978f4694ff0e6db4bf8716e5ac
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-04-13 07:23:49 -0300

    feat: create gameScreen class

---
+++ src/render/Screens/GameScreen.js
@@ -0,0 +1,39 @@
+import { Screen } from "./Screen.js";
+
+export default class GameScreen extends Screen {
+    constructor() {
+        super()
+
+        this.buildFunction = () => {
+            const game_screen = document.createElement('div')
+            game_screen.className = "screen-wrapper"
+
+            game_screen.innerHTML = `
+            <div id="game">
+                <canvas id="canvas" width="315" height="315"></canvas>
+                    <div id="canvas-top">
+                        <div class="line">
+                            <div>Pontuação:</div>
+                            <div id="points">0</div>
+                        </div>
+                        <div class="line">
+                            <div>Recorde:</div>
+                            <div id="record">0</div>
+                        </div>
+                </div>
+            </div>`
+
+            return game_screen
+        }
+
+        this.reset()
+    }
+
+    get components(){
+        return {
+            canvas: this.screen.querySelector('canvas'),
+            points_div: this.screen.querySelector("#points"),
+            record_div: this.screen.querySelector('#record')
+        }
+    }
+}
