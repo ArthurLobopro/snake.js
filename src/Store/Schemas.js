92515f32cb3ead07da30f043f607f6dc563899d2
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-08-22 20:32:19 -0300

    lint: run biome on Store

--- src/Store/Schemas.js
+++ src/Store/Schemas.js
@@ -1,6 +1,6 @@
 const colorType = {
     type: "string",
-    minLength: 4
+    minLength: 4,
 }
 
 const colorsSchema = {
@@ -9,60 +9,60 @@ const colorsSchema = {
         properties: {
             background: {
                 ...colorType,
-                readOnly: true
+                readOnly: true,
             },
             snake: {
                 ...colorType,
-                readOnly: true
+                readOnly: true,
             },
             snake_tail: {
                 ...colorType,
-                readOnly: true
-            }
+                readOnly: true,
+            },
         },
         default: {
             background: "#141414",
             snake: "#4AA96C",
-            snake_tail: "#9FE6A0"
-        }
+            snake_tail: "#9FE6A0",
+        },
     },
     colors: {
         type: "object",
         properties: {
             background: {
                 ...colorType,
-                default: "#141414"
+                default: "#141414",
             },
             snake: {
                 ...colorType,
-                default: "#4AA96C"
+                default: "#4AA96C",
             },
             snake_tail: {
                 ...colorType,
-                default: "#9FE6A0"
-            }
+                default: "#9FE6A0",
+            },
         },
         default: {
             background: "#141414",
             snake: "#4AA96C",
-            snake_tail: "#9FE6A0"
-        }
-    }
+            snake_tail: "#9FE6A0",
+        },
+    },
 }
 
 const dataSchema = {
     recorde: {
         type: "number",
         minimum: 0,
-        default: 0
-    }
+        default: 0,
+    },
 }
 
 const preferencesSchema = {
     velocity: {
         enum: [100, 200, 300],
-        default: 200
-    }
+        default: 200,
+    },
 }
 
 module.exports = { colorsSchema, dataSchema, preferencesSchema }
